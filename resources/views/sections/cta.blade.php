363031bd495ba5390728b453e4f1b21bd5b642d0
Abduhalim05 <93915359+Abduhalim05@users.noreply.github.com>
2022-07-29 14:42:37 +0300

    first step

---
+++ resources/views/sections/cta.blade.php
@@ -0,0 +1,26 @@
+<div class="cta-section padding">
+    <div class="container">
+      <div class="cta-content text-center">
+        <span class="wow fadeInUp">Explore The Features</span>
+        <h2 class="wow fadeInUp" data-wow-delay="300ms">The best constructions <br>company since 1985 </h2>
+        <a href="#" class="default-btn wow fadeInUp" data-wow-delay="500ms">Get Free Quote</a>
+      </div>
+    </div>
+  </div>
+  <section class="content-section padding">
+    <div class="container">
+      <div class="row content-wrap">
+        <div class="col-lg-6 sm-padding wow fadeInLeft" data-wow-delay="100ms">
+          <img class="box-shadow" class="box-shadow" src="img/content-1.jpg" alt="img">
+        </div>
+        <div class="col-lg-6 sm-padding">
+          <div class="content-info wow fadeInRight" data-wow-delay="300ms">
+            <span>Explore The Features</span>
+            <h2>Offering the most complete integrated package!</h2>
+            <p>We are committed to building a sustainable future by fostering a collaborative spirit that creates exceptional experiences, balanced relationships, and community built environment. Building isn’t just job. It's our passion. With every project we undertake, we set the bar high and provide the best industry.</p>
+            <a href="#" class="default-btn">Get Free Quote</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.site')
+
+@section('content')
+
+    @include('sections.slider')
+    @include('sections.about')
+    @include('sections.service')
+    @include('sections.workpro')
+    @include('sections.projects')
+    @include('sections.cta')
+    @include('sections.comments')
+    @include('sections.blog')
+    @include('sections.sponsor')
+
+@endsection
